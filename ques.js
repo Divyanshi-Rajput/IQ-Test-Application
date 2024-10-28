f2c000c68fb8a0f67a1f473ff925dabd5fb1af5b
Divyanshi Rajput <143251156+Divyanshi-Rajput@users.noreply.github.com>
2024-10-28 22:14:34 +0530

    Add files via upload

---
+++ ques.js
@@ -0,0 +1,393 @@
+export const questions = [
+  {
+    question: "If all bloops are razzies and all razzies are lazzies, are all bloops definitely lazzies?",
+    options: ["Yes", "No", "Maybe", "Not enough information"],
+    answer: "Yes"
+  },
+  {
+    question: "If it takes 5 machines 5 minutes to make 5 widgets, how long would it take 100 machines to make 100 widgets?",
+    options: ["5 minutes", "100 minutes", "50 minutes", "10 minutes"],
+    answer: "5 minutes"
+  },
+  {
+      question: "What is the average of the numbers 5, 8, 12, 14, and 21?",
+      options: ["10", "12", "15", "20"],
+      answer: "12"
+  },
+  {
+      question: "If 7x - 3 = 25, what is the value of x?",
+      options: ["2", "4", "5", "6"],
+      answer: "4"
+  },
+  {
+    question: "Which number completes the analogy: 4 is to 64 as 3 is to ___?",
+    options: ["16", "27", "36", "48"],
+    answer: "48"
+  },
+  {
+    question: "Which word is the odd one out: Cup, Plate, Bowl, Spoon?",
+    options: ["Cup", "Plate", "Bowl", "Spoon"],
+    answer: "Spoon"
+  },
+  {
+      question: "A shirt costs $35 after a 30% discount. What was the original price?",
+      options: ["$45", "$50", "$55", "$60"],
+      answer: "$50"
+  },
+  {
+    question: "If a car travels 60 miles in 1 hour, how far will it travel in 3 hours at the same speed?",
+    options: ["120 miles", "180 miles", "150 miles", "200 miles"],
+    answer: "180 miles"
+  },
+  {
+      question: "What is the sum of the first five prime numbers?",
+      options: ["18", "26", "28", "30"],
+     answer: "28"
+  },
+  {
+    question: "What is 15% of 200?",
+    options: ["15", "30", "45", "60"],
+    answer: "30"
+  },
+  {
+    question: "Which of the following can be divided evenly by 3: 112, 234, 513, 241?",
+    options: ["112", "234", "513", "241"],
+    answer: "234"
+  },
+  {
+    question: "If a rectangle has a length of 10 cm and a width of 4 cm, what is its area?",
+    options: ["14 cm²", "28 cm²", "40 cm²", "56 cm²"],
+    answer: "40 cm²"
+  },
+  {
+    question: "If you buy 3 books for $15 each and pay a sales tax of 10%, what is your total cost?",
+    options: ["$45", "$49.50", "$50", "$55"],
+    answer: "$49.50"
+  },
+  {
+    question: "If 3x + 5 = 20, what is the value of x?",
+    options: ["3", "4", "5", "6"],
+    answer: "5"
+  },
+  {
+    question: "What is the synonym of the word 'diligent'?",
+    options: ["Lazy", "Careless", "Hardworking", "Clumsy"],
+    answer: "Hardworking"
+  },
+  {
+    question: "What is the value of 2^5?",
+    options: ["16", "32", "64", "128"],
+    answer: "32"
+  },
+  {
+    question: "Which of the following is the odd one out: 2, 3, 7, 11, 13?",
+    options: ["2", "7", "11", "3"],
+    answer: "2"
+  },
+  {
+    question: "If all P are Q and some Q are R, then which of the following is true?",
+    options: ["Some P are R", "All R are P", "Some P are not R", "None of the above"],
+    answer: "None of the above"
+  },
+  {
+    question: "Which cube cannot be made based on the unfolded cube?",
+    questionImage: 'images/s1q.png',  
+    options: [
+      { imgSrc: 'images/s1a1.png', value: "a" },
+      { imgSrc: 'images/s1a2.png', value: "b" },
+      { imgSrc: 'images/s1a3.png', value: "c" },
+      { imgSrc: 'images/s1a4.png', value: "d" }
+    ],
+    answer: "b"
+  },
+  {
+    question: "Which of the figures would continue the series below?",
+    questionImage: 'images/s2q.png',  
+    options: [
+      { imgSrc: 'images/s2a1.png', value: "a" },
+      { imgSrc: 'images/s2a2.png', value: "b" },
+      { imgSrc: 'images/s2a3.png', value: "c" },
+      { imgSrc: 'images/s2a4.png', value: "d" }
+    ],
+    answer: "a"
+  },
+  {
+    question: "Complete the sentence: He was so tired that he could barely keep his eyes ___.",
+    options: ["Open", "Closed", "Wide", "Shut"],
+    answer: "Open"
+  },
+  {
+    question: "Which number comes next in the sequence: 2, 6, 12, 20, 30?",
+    options: ["42", "40", "44", "46"],
+    answer: "42"
+  },
+  {
+    question: "A store sells apples for $2 each. If you buy 8 apples, how much do you pay?",
+    options: ["$10", "$12", "$14", "$16"],
+    answer: "$16"
+  },
+  {
+    question: "Find the missing number: 2, 5, 10, 17, 26, ___?",
+    options: ["34", "35", "37", "38"],
+    answer: "37"
+  },
+  {
+    question: "Which word is the odd one out: Cat, Dog, Tiger, Leopard?",
+    options: ["Cat", "Dog", "Tiger", "Leopard"],
+    answer: "Dog"
+  },
+  {
+    question: "When put together properly, the top three puzzle pieces will create one of the following shapes (A-E). "<br>" Note that a side marked X has to touch X and a side marked Y has to touch Y.",
+    questionImage: 'images/s3q.png',  
+    options: [
+      { imgSrc: 'images/s3a1.png', value: "a" },
+      { imgSrc: 'images/s3a2.png', value: "b" },
+      { imgSrc: 'images/s3a3.png', value: "c" },
+      { imgSrc: 'images/s3a4.png', value: "d" }
+    ],
+    answer: "a"
+  },
+  {
+    question: "If it rains, the ground gets wet. It rained today. What can be inferred?",
+    options: ["The ground is wet", "The ground is dry", "The ground might be wet", "None of the above"],
+    answer: "The ground is wet"
+  },
+  {
+    question: "What is the meaning of the idiom 'Break the ice'?",
+    options: ["Break something", "End a conflict", "Start a conversation", "Freeze something"],
+    answer: "Start a conversation"
+  },
+  {
+      question: "How many minutes are there in 3.5 hours?",
+      options: ["180 minutes", "200 minutes", "210 minutes", "220 minutes"],
+      answer: "210 minutes"
+  },
+  {
+      question: "If the word 'CONSERVATION' is coded as 'NOITAVRESNOC', how would 'POLLUTION' be coded?",
+      options: ["NOITULLOP", "LOITUPOLL", "NOITULLO", "POTILLUNO"],
+      answer: "NOITULLOP"
+  },
+  {
+      question: "A is taller than B but shorter than C. D is shorter than A but taller than B. Who is the tallest?",
+      options: ["A", "B", "C", "D"],
+      answer: "C"
+  },
+  {
+    question: "Which of the following frepresents the cube in its folded form?",
+    questionImage: 'images/s4q.png',  
+    options: [
+      { imgSrc: 'images/s4a1.png', value: "a" },
+      { imgSrc: 'images/s4a2.png', value: "b" },
+      { imgSrc: 'images/s4a3.png', value: "c" },
+      { imgSrc: 'images/s4a4.png', value: "d" }
+    ],
+    answer: "c"
+  },
+  {
+    question: "Find the correct word: I was ___ to hear about the accident.",
+    options: ["displeased", "delighted", "shocked", "bored"],
+    answer: "shocked"
+  },
+  {
+      question: "If the day before yesterday was Wednesday, what day will it be the day after tomorrow?",
+      options: ["Monday", "Tuesday", "Wednesday", "Friday"],
+      answer: "Tuesday"
+  },
+  {
+      question: "If 2/3 of a number is 36, what is the number?",
+      options: ["36", "48", "54", "72"],
+      answer: "54"
+  },
+  {
+    question: "Which figure is the rotation of the given object?",
+    questionImage: 'images/s5q.png',  
+    options: [
+      { imgSrc: 'images/s5a1.png', value: "a" },
+      { imgSrc: 'images/s5a2.png', value: "b" },
+      { imgSrc: 'images/s5a3.png', value: "c" },
+      { imgSrc: 'images/s5a4.png', value: "d" }
+    ],
+    answer: "d"
+  },
+  {
+      question: "A man starts walking north, then turns right, then right again, and then right again. In which direction is he now facing?",
+      options: ["East", "West", "North", "South"],
+      answer: "South"
+  },
+  {
+    question: "What is the antonym of 'optimistic'?",
+    options: ["Pessimistic", "Hopeful", "Joyful", "Cheerful"],
+    answer: "Pessimistic"
+  },
+  {
+      question: "Which shape does not belong in this group: Square, Circle, Triangle, Pentagon?",
+      options: ["Square", "Circle", "Triangle", "Pentagon"],
+      answer: "Circle"
+  },
+  {
+    question: "Which figure is the top-down view of the given shape?",
+    questionImage: 'images/s6q.png',  
+    options: [
+      { imgSrc: 'images/s6a1.png', value: "a" },
+      { imgSrc: 'images/s6a2.png', value: "b" },
+      { imgSrc: 'images/s6a3.png', value: "c" },
+      { imgSrc: 'images/s6a4.png', value: "d" }
+    ],
+    answer: "b"
+  },
+  {
+    question: "What does the idiom 'spill the beans' mean?",
+    options: ["Make a mess", "Cook beans", "Reveal a secret", "Cry loudly"],
+    answer: "Reveal a secret"
+  },
+  {
+      question: "If X is the brother of Y and Y is the sister of Z, how is Z related to X?",
+      options: ["Brother", "Sister", "Cousin", "Unrelated"],
+      answer: "Brother"
+  },
+  {
+      question: "What is 20% of 250?",
+      options: ["25", "50", "75", "100"],
+      answer: "50"
+  },
+  {
+      question: "If Jane is standing behind Mary and Mary is standing behind Peter, who is standing in front?",
+      options: ["Jane", "Mary", "Peter", "All are standing together"],
+      answer: "Peter"
+  },
+  {
+    question: "Choose the best word to complete the sentence: The ___ cat jumped over the fence.",
+    options: ["small", "tiny", "enormous", "huge"],
+    answer: "small"
+  },
+    {
+        question: "If you rearrange the letters 'CIFAIPC', you would get the name of a:",
+        options: ["City", "Animal", "Ocean", "Country"],
+        answer: "Ocean"
+    },
+    {
+        question: "If you save $50 each month, how much will you have saved after 8 months?",
+        options: ["$200", "$300", "$400", "$500"],
+        answer: "$400"
+    },
+    {
+      question: "how many four sided images does this diagram contain?",
+      questionImage: 'images/s7q.png',  
+      options: ["12-15", "16-21", "22-30", "30-35"],
+      answer: "22-30"
+    },
+    {
+      question: "What is the square root of 144?",
+      options: ["10", "11", "12", "13"],
+      answer: "12"
+    },
+    {
+      question: "Which of the following would follow in the given sequence?",
+      questionImage: 'images/s8q.png',  
+      options: [
+        { imgSrc: 'images/s8a1.png', value: "a" },
+        { imgSrc: 'images/s8a2.png', value: "b" },
+        { imgSrc: 'images/s8a3.png', value: "c" },
+        { imgSrc: 'images/s8a4.png', value: "d" }
+      ],
+      answer: "b"
+    },
+    {
+        question: "A clock shows 3:30. What is the angle between the hour and the minute hands?",
+        options: ["75 degrees", "90 degrees", "105 degrees", "60 degrees"],
+        answer: "75 degrees"
+    },
+    {
+      question: "Which of the following would follow in the given sequence?",
+      questionImage: 'images/s11q.png',  
+      options: [
+        { imgSrc: 'images/s11a1.png', value: "a" },
+        { imgSrc: 'images/s11a2.png', value: "b" },
+        { imgSrc: 'images/s11a3.png', value: "c" },
+        { imgSrc: 'images/s11a4.png', value: "d" }
+      ],
+      answer: "a"
+    },
+    {
+        question: "Which of the following can be arranged into a 5-letter English word: RAHMO?",
+        options: ["MORAH", "HOARM", "ROMAH", "HORAM"],
+        answer: "MORAH"
+    },
+    {
+        question: "What is the result of 9 x 9 + 9?",
+        options: ["81", "90", "99", "108"],
+        answer: "90"
+    },
+    {
+      question: "Which word is the odd one out: Apple, Orange, Banana, Car?",
+      options: ["Apple", "Orange", "Banana", "Car"],
+      answer: "Car"
+    },
+    {
+      question: "What is the synonym of the word 'arrogant'?",
+      options: ["Humble", "Modest", "Proud", "Kind"],
+      answer: "Proud"
+    },
+    {
+      question: "Complete the sentence: She is always ___ to new ideas.",
+      options: ["open", "closed", "indifferent", "angry"],
+      answer: "open"
+    },
+    {
+        question: "What comes next in the pattern: A1, B2, C3, D4, E5, ___?",
+        options: ["F6", "F5", "G5", "E6"],
+        answer: "F6"
+    },
+    {
+      question: "Which word is the antonym of 'confident'?",
+      options: ["Shy", "Bold", "Assured", "Certain"],
+      answer: "Shy"
+    },
+    {
+      question: "Which of the following would follow in the given sequence?",
+      questionImage: 'images/s10q.png',  
+      options: [
+        { imgSrc: 'images/s10a1.png', value: "a" },
+        { imgSrc: 'images/s10a2.png', value: "b" },
+        { imgSrc: 'images/s10a3.png', value: "c" },
+        { imgSrc: 'images/s10a4.png', value: "d" }
+      ],
+      answer: "d"
+    },
+    {
+        question: "Which one of the following is an odd number: 2, 4, 6, 9?",
+        options: ["2", "4", "6", "9"],
+        answer: "9"
+    },
+    {
+        question: "If a train travels 100 miles in 2 hours, how far will it travel in 5 hours at the same speed?",
+        options: ["200 miles", "250 miles", "300 miles", "350 miles"],
+        answer: "250 miles"
+    },
+    {
+      question: "Which of the following would follow in the given sequence?",
+      questionImage: 'images/s9q.png',  
+      options: [
+        { imgSrc: 'images/s9a1.png', value: "a" },
+        { imgSrc: 'images/s9a2.png', value: "b" },
+        { imgSrc: 'images/s9a3.png', value: "c" },
+        { imgSrc: 'images/s9a4.png', value: "d" }
+      ],
+      answer: "a"
+    },
+    {
+      question: "which word is spelled incorrectly?",
+      options: ["Accomodate", "Accept", "Access", "Across"],
+      answer: "Accomodate"
+    },
+    {
+      question: "Choose the largest number?",
+      options: ["0.5", "5.00", "0.055", "0.0505"],
+      answer: "5.00"
+    },
+    {
+      question: "if the statement 'all birds can fly' is false, "<br>"which of the following must be true?",
+      options: ["No birds can fly", "Some birds can fly", "All birds cannot fly", "Cannot be determined"],
+      answer: "Some birds can fly"
+    }
+];
